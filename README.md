2952788e48eec6328bb87da485fce6530088b753
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-05-04 21:43:05 +0600

    about update

--- README.md
+++ README.md
@@ -58,3 +58,29 @@ Currently, two official plugins are available:
                     </div>
                 </div> 
         </div>
+
+### Create About Section With Image Relative Position 
+            import personal from '../../assets/images/about_us/person.jpg';
+            import parts from '../../assets/images/about_us/parts.jpg';
+
+            const About = () => {
+            return (
+                 <div className="hero min-h-screen bg-base-200">
+                    <div className="hero-content flex-col lg:flex-row">
+                        <div className='lg-w-1/3 min-w-[200px] relative'>
+
+                            <img src={personal} className="w-3/4 rounded-lg shadow-2xl" />
+                            <img src={parts} className="w-1/2 absolute right-5 top-1/2 border-8 border-white rounded-lg shadow-2xl" />
+                </div>
+                    <div className='lg-w-1/2'>
+                    <h3 className='text-3xl text-orange-500'>About</h3>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+                </div>
+                );
+                };
+
+                export default About;
--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -4,15 +4,16 @@ import parts from '../../assets/images/about_us/parts.jpg';
 const About = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row">
-    <div className='lg-w-1/3 min-w-[200px] relative'>
+  <div className="hero-content flex-col lg:flex-row p-10">
+    <div className='lg-w-1/2 relative'>
 
-    <img src={personal} className="w-3/4 rounded-lg shadow-2xl" />
-    <img src={parts} className="w-1/2 absolute right-5 top-1/2 border-8 border-white rounded-lg shadow-2xl" />
+        <img src={personal} className="w-3/4 rounded-lg shadow-2xl" />
+        <img src={parts} className="w-1/2 absolute right-5 top-1/2 border-8 border-white rounded-lg shadow-2xl" />
     </div>
     <div className='lg-w-1/2'>
-      <h3 className='text-3xl text-orange-500'>About</h3>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <h3 className='text-3xl text-orange-500 font-bold'>About</h3>
+      <h1 className="text-5xl font-bold">We are qualified & of Experience in this field</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
